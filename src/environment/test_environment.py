20c6ee0cf3108531bcd5436ae4231cffbe30161e
brunocampos01 <brunocampos01@gmail.com>
2019-06-26 14:04:02 -0300

    refactor

--- src/environment/test_environment.py
+++ src/environment/test_environment.py
@@ -15,9 +15,8 @@ def main():
             "Unrecognized python interpreter: {}".format(REQUIRED_PYTHON))
 
     if system_major != required_major:
-        raise TypeError(
-            "This project requires Python {}. Found: Python {}"
-                .format(required_major, sys.version))
+        raise TypeError("This project requires Python {}. Found: Python {}"
+                        .format(required_major, sys.version))
     else:
         print(">>> Development environment passes all tests!")
 
