64cad2ef424ac181e908e3ce80b27ecf8cf91d72
brunocampos01 <brunocampos01@gmail.com>
2019-07-05 16:01:40 -0300

    refactor: update scripts and notebooks

--- src/etl_PETR4.py
+++ src/etl_PETR4.py
@@ -7,39 +7,46 @@ import requests
 from bs4 import BeautifulSoup
 from sqlalchemy import create_engine
 
-# construção da URL
-# datas
+# Global
 year = datetime.date.today().year
 month = datetime.date.today().month
 day = datetime.date.today().day
 data_inicial = (f'{day}/{month}/{year - 10}')
 data_final = (f'{day}/{month}/{year}')
-
-# ativo
 ativo = 'PETR4'
-
-# url
-response = requests. \
-    get(f"https://www.infomoney.com.br/Pages/Download/Download.aspx?"
-        f"dtIni={data_inicial}&dtFinish={data_final}"
-        f"&Ativo={ativo}"
-        f"&Semana=null"
-        f"&Per=null"
-        f"&type=2"
-        f"&Stock={ativo}"
-        f"&StockType=1",
-        timeout=60)
-print("get page: ", response)
-
-# criação do parser para navegação na árvore DOM
-content = BeautifulSoup(response.content, "html.parser")
-table = content.find_all("tr")
+url = (f"https://www.infomoney.com.br/Pages/Download/Download.aspx?"
+       f"dtIni={data_inicial}&dtFinish={data_final}"
+       f"&Ativo={ativo}"
+       f"&Semana=null"
+       f"&Per=null"
+       f"&type=2"
+       f"&Stock={ativo}"
+       f"&StockType=1")
 
 
-# Removing Multple spaces
+def prepare_url(url: str):
+    """
+    Construção da URL
+    :return: http response
+    """
+    response = requests.get(url, timeout=60)
+
+    print("get page: ", response)
+    return response
+
+
+def create_parser(response: str):
+    """
+    Criação do parser para navegação na árvore DOM
+    :return: bs4.element
+    """
+    content = BeautifulSoup(response.content, "html.parser")
+    return content.find_all("tr")
+
+
 def remove_multiple_spaces(string):
     """
-    Se é String, então faz remoção de espaços em branco 
+    Se é String, então faz remoção de espaços em branco
     replace: / por espaço em branco
     replace: , por . para facilitar a conversão de tipos
     replace: todas as palavras que entraram na tabela
@@ -48,74 +55,81 @@ def remove_multiple_spaces(string):
         string_n = ' '.join(string.split()) \
             .replace('/', ' ') \
             .replace(',', '.')
+
         string = string_n \
             .replace('Fech.', '-1') \
             .replace('Var.Dia',
                      '-1')
         return string
+
     return string
 
 
-"""
-Contrução da tabela
-"""
-# neste dataframe vou armazenar a data e o fechamento diário
-df_petr4 = pd.DataFrame(columns=['Ano', 'Fechamento'])
+def generate_table(table: 'bs4.element.ResultSet'):
+    # neste dataframe vou armazenar a data e o fechamento diário
+    df_petr4 = pd.DataFrame(columns=['Ano', 'Fechamento'])
 
-# percorre table
-for row in table:
-    # .text vem do requests e garante o scrapping
-    text = row.text
-    text = remove_multiple_spaces(text)
+    # percorre tabela
+    for row in table:
+        # .text vem do requests e garante o scrapping
+        text = row.text
+        text = remove_multiple_spaces(text)
 
-    # insere dados diferentes a cada linha
-    text_dados = text.split(sep=" ")
+        # insere dados diferentes a cada linha
+        text_dados = text.split(sep=" ")
 
-    # convert list to String
-    # quando é feito o slice do text_dados é retornado um list
-    ano = np.asarray(text_dados[2:3])
-    fechamento = np.asarray(text_dados[3:4])
+        # quando é feito o slice do text_dados é retornado um list
+        ano = np.asarray(text_dados[2:3])
+        fechamento = np.asarray(text_dados[3:4])
 
-    # variável p organizar a inserção no dataframe
-    dados = pd.DataFrame([[ano, fechamento]],
-                         columns=['Ano', 'Fechamento'])
-    df_petr4 = df_petr4.append(dados)
+        # variável p organizar a inserção no dataframe
+        dados = pd.DataFrame([[ano, fechamento]],
+                             columns=['Ano', 'Fechamento'])
+        df_petr4 = df_petr4.append(dados)
 
-# remove rows with Strings
-df_petr4 = df_petr4[1:]
+    # remove linhas com Strings
+    df_petr4 = df_petr4[1:]
 
-# Conversão de tipos object
-df_petr4.Ano = df_petr4.Ano.astype(
-    'int16')  # int8 não aceitou, pequeno demais
-df_petr4.Fechamento = df_petr4.Fechamento.astype('float16')
+    # Conversão de tipos object
+    df_petr4.Ano = df_petr4.Ano.astype(
+        'int16')  # int8 não aceitou, pequeno demais
+    df_petr4.Fechamento = df_petr4.Fechamento.astype('float16')
 
-""" Granularidade anual: 
-- tanto a taxa selic quanto a PETR4 devem ser de mesma granularidade
-- nível do grão = ano
-"""
-# dataframe somente com os fechamentos do ano
-df_petr4_clean = pd.DataFrame(columns=['Ano', 'Fechamento'])
+    """ Granularidade anual: 
+    - tanto a taxa selic quanto a PETR4 devem ser de mesma granularidade
+    - nível do grão = ano
+    """
+    # dataframe somente com os fechamentos do ano
+    df_petr4_clean = pd.DataFrame(columns=['Ano', 'Fechamento'])
 
-# for serve para get fechamento do ano
-for ano in range(year - 9, year + 1):
-    # print(ano)
-    df_petr4_year = df_petr4[(df_petr4['Ano'] == ano)]
+    # for serve para get fechamento do ano
+    for ano in range(year - 9, year + 1):
+        df_petr4_year = df_petr4[(df_petr4['Ano'] == ano)]
+        fechamento_ano = df_petr4_year.Fechamento.iloc[0]
 
-    # get value 
-    fechamento_ano = df_petr4_year.Fechamento.iloc[0]
-    # print(fechamento_ano)
+        # variável p organizar a inserção no dataframe
+        dados = pd.DataFrame([[ano, fechamento_ano]],
+                             columns=['Ano', 'Fechamento'])
+        df_petr4_clean = df_petr4_clean.append(dados)
 
-    # variável p organizar a inserção no dataframe
-    dados = pd.DataFrame([[ano, fechamento_ano]],
-                         columns=['Ano', 'Fechamento'])
-    df_petr4_clean = df_petr4_clean.append(dados)
+    print(df_petr4_clean.info())
+    return df_petr4_clean
 
-print(df_petr4_clean.info())
 
-# Salvamento do dataframe no sqlite
-engine = create_engine('sqlite:///desafio_AAWZ.db')
-df_petr4_clean.to_sql('petr4',
-                      con=engine,
-                      if_exists='replace',
-                      index=False)
-print('\n(petr4.db) salvo!')
+def dataframe_to_sqlite(ativo: str, dataframe: 'dataframe'):
+    engine = create_engine('sqlite:///data/desafio_AAWZ.db')
+    dataframe.to_sql(ativo, con=engine,
+              if_exists='replace',
+              index=False)
+    print(f'\n{ativo} salvo!')
+
+
+def main():
+    response = prepare_url(url=url)
+    parser_bs = create_parser(response)
+    df = generate_table(parser_bs)
+    dataframe_to_sqlite(ativo=ativo, dataframe=df)
+
+
+if __name__ == '__main__':
+    main()
--- src/etl_SELIC.py
+++ src/etl_SELIC.py
@@ -7,29 +7,44 @@ import requests
 from bs4 import BeautifulSoup
 from sqlalchemy import create_engine
 
-response = requests.get(
-    "https://www.bcb.gov.br/pec/copom/port/taxaselic.asp",
-    timeout=60)
-print("get page: ", response)
 
-# criação do parser para navegação na árvore DOM
-content = BeautifulSoup(response.content, "html.parser")
-table = content.find_all("tr")
+url = "https://www.bcb.gov.br/controleinflacao/historicotaxasjuros"
+ativo = 'SELIC'
 
 
-## Removing Multple spaces
+def prepare_url(url: str):
+    """
+    Construção da URL
+    :return: http response
+    """
+    response = requests.get(url, timeout=60)
+
+    print("get page: ", response)
+    return response
+
+
+def create_parser(response: str):
+    """
+    Criação do parser para navegação na árvore DOM
+    :return: bs4.element
+    """
+    content = BeautifulSoup(response.content, "html.parser")
+
+    return content.find_all("td")
+
+
 def remove_multiple_spaces(string):
     """
-    Se é String, então faz remoção de espaços em branco 
-    trans_string: remove os valores textuais por espaços em branco 
+    Se é String, então faz remoção de espaços em branco
+    trans_string: remove os valores textuais por espaços em branco
     trans_n: / por espaço em branco AND (,) por (.) para facilitar a conversão de tipos
     """
     if type(string) == str:
         trans_string = string \
-            .replace('baixa', ' ') \
-            .replace('alta', ' ') \
-            .replace(' ex.', '') \
-            .replace('uso', ' ')
+                            .replace('baixa', ' ') \
+                            .replace('alta', ' ') \
+                            .replace(' ex.', '') \
+                            .replace('uso', ' ')
 
         divisao = ' '.join(trans_string.split())
         trans_n = divisao.replace('/', ' ').replace(',', '.')
@@ -37,69 +52,82 @@ def remove_multiple_spaces(string):
     return string
 
 
-"""
-Contrução da tabela
-"""
-# neste dataframe vou armazenar a data e a taxa SELIC
-df_selic = pd.DataFrame(columns=['Ano', 'Taxa SELIC'])
+def generate_table(table: 'bs4.element.ResultSet'):
+    """
+    Contrução da tabela
+    """
+    df_selic = pd.DataFrame(columns=['Ano', 'Taxa SELIC'])
 
-# percorre table
-for row in table:
-    text = row.text
-    text = remove_multiple_spaces(text)
+    print('\n')
+    print(table)
 
-    # insere dados diferentes a cada linha
-    text_dados = text.split(sep=" ")
+    for row in table:
+        text = row.text
+        text = remove_multiple_spaces(text)
 
-    # convert list to String
-    # quando é feito o slice do text_dados é retornado um list
-    ano = np.asarray(text_dados[6:7])
-    taxa = np.asarray(text_dados[13:])
+        # insere dados diferentes a cada linha
+        text_dados = text.split(sep=" ")
 
-    # DataFrame p organizar a inserção no dataframe
-    dados = pd.DataFrame([[ano, taxa]], columns=['Ano', 'Taxa SELIC'])
-    df_selic = df_selic.append(dados)
+        # convert list to String
+        # quando é feito o slice do text_dados é retornado um list
+        ano = np.asarray(text_dados[6:7])
+        taxa = np.asarray(text_dados[13:])
 
-# drop rows with Strings
-df_selic = df_selic[3:]
+        # DataFrame p organizar a inserção no dataframe
+        dados = pd.DataFrame([[ano, taxa]], columns=['Ano', 'Taxa SELIC'])
+        df_selic = df_selic.append(dados)
 
-# select 10 years
-year = (datetime.date.today().year)
-data_inicial = (f'{year - 10}')
-df_selic = df_selic[df_selic.Ano >= data_inicial]
+    print(df_selic)
+    # drop rows with Strings
+    df_selic = df_selic[3:]
 
-# Conversão de tipos object
-df_selic.Ano = df_selic.Ano.astype(
-    'int16')  # int8 não aceitou, pequeno demais
-df_selic['Taxa SELIC'] = df_selic['Taxa SELIC'].astype('float16')
+    # select 10 years
+    year = (datetime.date.today().year)
+    data_inicial = (f'{year - 10}')
+    df_selic = df_selic[df_selic.Ano >= data_inicial]
 
-""" Granularidade anual: 
-- tanto a taxa selic quanto a PETR4 devem ser de mesma granularidade
-- nível do grão = ano
-"""
-# dataframe somente com as taxas de fechamento de cada ano
-df_selic_clean = pd.DataFrame(columns=['Ano', 'Taxa SELIC'])
+    # Conversão de tipos object
+    df_selic.Ano = df_selic.Ano.astype('int16')
+    df_selic['Taxa SELIC'] = df_selic['Taxa SELIC'].astype('float16')
 
-# for serve para get fechamento do ano
-for ano in range(year - 9, year + 1):
-    # print(ano)
-    df_selic_year = df_selic[(df_selic['Ano'] == ano)]
+    """ Granularidade anual: 
+    - tanto a taxa selic quanto a PETR4 devem ser de mesma granularidade
+    - nível do grão = ano
+    """
+    # dataframe somente com as taxas de fechamento de cada ano
+    df_selic_clean = pd.DataFrame(columns=['Ano', 'Taxa SELIC'])
 
-    # get value 
-    taxa_ano = df_selic_year['Taxa SELIC'].iloc[0]
-    # print(taxa_ano)
+    # for serve para get fechamento do ano
+    for ano in range(year - 9, year + 1):
+        # print(ano)
+        df_selic_year = df_selic[(df_selic['Ano'] == ano)]
 
-    # variável p organizar a inserção no dataframe
-    dados = pd.DataFrame([[ano, taxa_ano]],
-                         columns=['Ano', 'Taxa SELIC'])
-    df_selic_clean = df_selic_clean.append(dados)
+        # get value
+        taxa_ano = df_selic_year['Taxa SELIC'].iloc[0]
+        # print(taxa_ano)
 
-print(df_selic_year.info())
+        # variável p organizar a inserção no dataframe
+        dados = pd.DataFrame([[ano, taxa_ano]],
+                             columns=['Ano', 'Taxa SELIC'])
+        df_selic_clean = df_selic_clean.append(dados)
 
-# Salvamento do dataframe no sqlite
-engine = create_engine('sqlite:///desafio_AAWZ.db')
-df_selic_clean.to_sql('selic',
-                      con=engine,
-                      if_exists='replace',
-                      index=False)
-print('\n(selic.db) salvo!')
+    return df_selic_year
+
+
+def dataframe_to_sqlite(ativo: str, dataframe: 'dataframe'):
+    engine = create_engine('sqlite:///data/desafio_AAWZ.db')
+    dataframe.to_sql(ativo, con=engine,
+                     if_exists='replace',
+                     index=False)
+    print(f'\n{ativo} salvo!')
+
+
+def main():
+    response = prepare_url(url=url)
+    parser_bs = create_parser(response)
+    df = generate_table(parser_bs)
+    dataframe_to_sqlite(ativo=ativo, dataframe=df)
+
+
+if __name__ == '__main__':
+    main()
